8015265a207750f984f06ebfc9493253615f8344
gautamaPopados <155830875+gautamaPopados@users.noreply.github.com>
2024-10-05 23:16:49 +0700

    Initial commit

---
+++ src/main/java/com/test/testtaskback/TestTaskBackApplication.java
@@ -0,0 +1,13 @@
+package com.test.testtaskback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/testtaskback/TestTaskBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.testtaskback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
